b881fbba3ab683cc938d7a6b4a4376a5b97a5d20
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-08-02 11:48:24 -0600

    java updata

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 let apiURL = 'https://api.imgflip.com/get_memes'
 
+let likeButton = document.querySelector('.like')
+let dislikeButton = document.querySelector('.dislike')
+let makeAMemeButton = document.querySelector('.makeAMeme')
+
+//variable to access the name to change with the new meme
+let memeHeader = document.querySelector('h2')
+
 let loadApiImage = () => {
     fetch(apiURL)
     .then(res => res.json())
@@ -8,8 +15,15 @@ let loadApiImage = () => {
         imageInfo(memeData)
     })
 }
+
 likeButton.addEventListener('click',()=>{
 })
+
+
+makeAMemeButton.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
+
 
 dislikeButton.addEventListener('click',()=>{
 
