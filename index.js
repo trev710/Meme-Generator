d8b1f2a4e1d538a5fad2f36ef1734657bc61ff10
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-08-02 12:05:06 -0600

    java up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let apiURL = 'https://api.imgflip.com/get_memes'
 
 let likeButton = document.querySelector('.like')
 let dislikeButton = document.querySelector('.dislike')
-let makeAMemeButton = document.querySelector('.makeAMeme')
+let generateNewMeme = document.querySelector('.makeAMeme')
 
 //variable to access the name to change with the new meme
 let memeHeader = document.querySelector('h2')
@@ -17,16 +17,27 @@ let loadApiImage = () => {
 }
 
 likeButton.addEventListener('click',()=>{
+//on click we want to increase a counter
+//we want to patch the likes with a count to the api list
+
+//maybe add color change on click
 })
 
 
-makeAMemeButton.addEventListener('submit', (e) => {
+generateNewMeme.addEventListener('submit', (e) => {
     e.preventDefault()
+//on submit we want to fetch a new meme from the api
+//we want to append the name to the h2 reference
+//we want to make the image data appear on the image box
 })
 
 
 dislikeButton.addEventListener('click',()=>{
+//on click we want to invoke the generate new meme function
+//we also want to remove the current meme from the api list
 
+
+//maybe add color changes
 })
 
 
